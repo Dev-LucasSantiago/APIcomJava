8dcabc34fc773d8b3e92715026f0b4ebeb3be5ba
Lucas Santiago <lucas_souza_s@hotmail.com>
2023-04-30 11:08:40 -0300

    Update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception{
+        //Fazer uma conexão  HTTP e buscar os top 250 filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_00qo4tak";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+
+        //Extrair os dados que interessam (Título, Poster, Classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        //Exibir e manipular dados
+        for (Map<String,String> filme : listaDeFilmes){
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
     }
 }
---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,11 @@
+public class GeradoraDeFigurinhas {
+    //Leituda da imagem
+    
+    //cria imagem nova com transparência
+
+    //Copiar a imagem original para novo imagem (em memória)
+
+    //Escrever uma frase em nova imagem
+
+    //Escrever a nova imagem em arquivo
+}
