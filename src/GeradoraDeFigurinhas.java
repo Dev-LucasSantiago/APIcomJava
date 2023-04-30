4dc0983d8b74011fa63cf80cac297568298c2ca9
Lucas Santiago <lucas_souza_s@hotmail.com>
2023-04-30 15:12:55 -0300

    Update

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,11 +1,38 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+
 public class GeradoraDeFigurinhas {
-    //Leituda da imagem
-    
-    //cria imagem nova com transparência
 
-    //Copiar a imagem original para novo imagem (em memória)
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception{
+        //Leitura da imagem
+        //InputStream inputStream =
+        // new FileInputStream("entrada/filme.jpg");
+        //InputStream inputStream =
+        // new URL("https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
-    //Escrever uma frase em nova imagem
+        //cria imagem nova com transparência
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
-    //Escrever a nova imagem em arquivo
+        //Copiar a imagem original para novo imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0,0,null);
+
+        //Configurar a fonte
+        Font fonte = new Font(Font.SANS_SERIF,Font.BOLD,128);
+        graphics.setColor(Color.yellow);
+        graphics.setFont(fonte);
+
+        //Escrever uma frase em nova imagem
+        graphics.drawString("TOPZERA",300,novaAltura - 50);
+
+        //Escrever a nova imagem em arquivo
+        ImageIO.write(novaImagem,"png",new File("saida/"+nomeArquivo));
+    }
 }
